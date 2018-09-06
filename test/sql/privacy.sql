77de5718e4ea340b8daf500fdbb7a7d40c716f8b
Amy Tai <amy@timescale.com>
2018-09-06 10:58:33 -0400

    Disabling default telemetry job in tests
    Turning off telemetry GUC so that the telemetry job doesn't ping our endpoint during regression
    tests. Also removes nondeterminism in tests that rely on state that the telemetry BGW might have
    changed.

--- test/sql/privacy.sql
+++ test/sql/privacy.sql
@@ -3,6 +3,7 @@ CREATE OR REPLACE FUNCTION _timescaledb_internal.test_privacy() RETURNS VOID
     AS :MODULE_PATHNAME, 'test_privacy' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 \c single :ROLE_DEFAULT_PERM_USER
 
+-- Should be off be default in tests
 SHOW timescaledb.telemetry_level;
 SET timescaledb.telemetry_level=off;
 SHOW timescaledb.telemetry_level;
@@ -10,14 +11,6 @@ SELECT _timescaledb_internal.test_privacy();
 -- To make sure nothing was sent, we check the UUID table to make sure no exported UUID row was created
 SELECT COUNT(*) from _timescaledb_catalog.installation_metadata;
 
--- Now try to send something
-RESET timescaledb.telemetry_level;
--- We don't care about the output of the version checker
-SET client_min_messages=error;
+-- Do it again to make sure
 SELECT _timescaledb_internal.test_privacy();
-RESET client_min_messages;
--- We know it attempted to send if there generated UUIDs
-SELECT COUNT(*) from _timescaledb_catalog.installation_metadata where key='uuid';
-SELECT COUNT(*) from _timescaledb_catalog.installation_metadata where key='exported_uuid';
-SELECT COUNT(*) from _timescaledb_catalog.installation_metadata where key='install_timestamp';
 SELECT COUNT(*) from _timescaledb_catalog.installation_metadata;
